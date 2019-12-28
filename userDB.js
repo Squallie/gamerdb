f9543397730b9931797e0f6fa03013042ff8cd1c
Squallie <squallieboiii@gmail.com>
2019-12-29 10:18:56 +1100

    Added passport to userdb
    + added crypto for hashing password
    + added a function for hashing
    + added a new method for authentication
    
    I believe the issue with login auth is coming from the html page rather than the function

--- userDB.js
+++ userDB.js
@@ -1,3 +1,4 @@
+//-------------MODULES-------------------------/
 //Adding Express Module
 const express = require('express')
 const app = express()
@@ -5,11 +6,16 @@ const app = express()
 //Adds module for Sqlite3
 const sqlite3 = require('sqlite3').verbose();
 
-//Importing passportjs
-const passport = require('./passport.js')
+//Adds module for passport.js
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
 
-//Creating Database Object //
-//Selects the Database to connect to.
+//Adds the Crypto Module
+const crypto = require('crypto')
+//---------------------------------------------/
+
+/*Creating Database Object 
+ Selects the Database to connect to. */
 let db = new sqlite3.Database('./Users.db', err => {
     if (err){
         //Returns log if unsuccessful
@@ -19,37 +25,57 @@ let db = new sqlite3.Database('./Users.db', err => {
     console.log("Connected to UserDB Successfully\n");
 });
 
-let sql = 'SELECT Username username, Password password FROM Users'
+//Function to hash password
+function hashPassword(password, Livi) {
+	const hash = crypto.createHash('sha256');
+	hash.update(password);
+	hash.update(Livi);
+	return hash.digest('hex');
+}
 
-/*Query Data
-needs to be fixed up
-db.get(sql, [Username], (err, rows) => {
-	if(err) {
-		return console.error(err.message);
-	}
-	return row
-	? console.log(row.username)
-	: console.log(`User ${Username} does not exist`)
-});
-*/
+//Used to search db to query data
+passport.use(new LocalStrategy((username, password, done) => {
+	db.get('SELECT Livi From Users WHERE Username = ?', username, (err, row) => {
+		if(!row){
+			return done(null, false);
+			var hash = hashPassword(password, row.Livi);
+			db.get('SELECT Username from Users WHERE Username = ? AND Password = ?', username, hash,
+			(err,rows) =>{
+			if(!row){
+				return done(null, false);
+			}
+			return done(null, rows);
+			});
+		}
+	});
+    }
+));
 
-/*Checking login OLD METHOD
-app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
-		db.query('SELECT * FROM Users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');s
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
-}); */
+//Serialize User
+passport.serializeUser((user, done) => {
+	return done(null, user.id)
+})
+
+//Deserialize User
+passport.deserializeUser((id, done) =>{
+	db.get('SELECT id, username FROM users WHERE id = ?', id, function(err, row) {
+		if (!row) return done(null, false);
+		return done(null, row);
+	  });
+})
+
+
+//Passport Authentication
+app.post('/auth', passport.authenticate('local', {
+    failureRedirect: '/login',
+    succussRedirect: '/'
+}));
+
+
+function isLoggedin(req, res, next){
+    if(req.isauthenticated()){
+        return next();
+    } else {
+        return res.redirect('/login');
+    }
+}
